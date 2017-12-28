e4c9c374b45fccb752497451f2d4af6f80184c3a
davalarcon <zilentstorm@gmail.com>
2017-12-28 10:54:24 -0500

    PHP-CLASS Working on new language

---
+++ embed.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+
+       <?php $title= "David Alarcon Site"; ?>
+
+    <h1>
+        <?php echo $title; ?>
+    </h1>
+    
+    </body>
+</html>
---
+++ variables.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+    <?php
+
+      $name = "David";
+      $age = 36;
+
+      echo "<h1> $name is $age years old.</h1>";
+
+      echo $name . ' is ' . $age . ' years old.';
+
+    ?>
+    
+    </body>
+</html>
